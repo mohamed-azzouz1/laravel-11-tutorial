a7338517f246b3179572463724dc38ef6335fe12
mohamed-azzouz1 <339078@student.mboutrecht.nl>
2024-10-20 16:39:16 +0200

    video 18 klaar

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,8 +3,12 @@
         job
 </x-slot>
     
-<h2 class="font-bold text-lg">{{ $job['title']}}</h2>
+<h2 class="font-bold text-lg">{{ $job->title}}</h2>
 <p>
-    this job pays {{$job['salary'] }} per year.
+    this job pays {{$job->salary }} per year.
+</p>
+
+<p class="mt-6">
+   <x-button href="/jobs/{{ $job->id}}/edit">Edit job</x-button>
 </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//index
 Route::get('/jobs', function ()  {
     $jobs = job::with('employer')->latest()->simplePaginate(3);
     
@@ -28,19 +29,19 @@ Route::get('/jobs', function ()  {
     ]);
 });
 
-
+//create
 route::get('/jobs/create', function(){
     return view('jobs.create');
 });
 
-
+//show
 Route::get('/jobs/{id}', function ($id)  {        
     $job = job::find($id);
     
     return view('jobs.show', ['job' => $job]);
 });
 
-
+//store
 route::post('/jobs', function(){
     request()->validate([
         'title' =>['required', 'min:3'],
@@ -57,6 +58,44 @@ route::post('/jobs', function(){
     return redirect('/jobs');
 });
 
+//edit
+Route::get('/jobs/{id}/edit', function ($id)  {        
+    $job = job::find($id);
+    
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//update
+Route::patch('/jobs/{id}', function ($id)  {        
+    
+    request()->validate([
+        'title' =>['required', 'min:3'],
+        'salary'=>['required']
+    ]);
+    //authorize
+
+    $job = job::findorfail($id);
+
+    // $job->title =request('title');
+    // $job->salary =request('salary');
+    // $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id );
+
+});
+
+//destroy
+Route::delete('/jobs/{id}', function ($id)  {        
+    
+    $job = Job::findorfail($id)->delete();
+
+    return redirect('/jobs');
+});
 
 
 

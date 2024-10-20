356f4248c6a9533d488f9b8b7dc600d0745f7c4c
mohamed-azzouz1 <339078@student.mboutrecht.nl>
2024-10-20 15:35:56 +0200

    video 17 klaar

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,10 @@ Route::get('/jobs/{id}', function ($id)  {
 
 
 route::post('/jobs', function(){
-    //validation
+    request()->validate([
+        'title' =>['required', 'min:3'],
+        'salary'=>['required']
+    ]);
 
     Job::create([
         'title' => request('title'),
